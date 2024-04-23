884e9be49b9fb98a4e8f7aee45ee5b45b60505f5
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2024-04-23 09:55:15 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Tailwind Config
+tailwind.config = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-bg': "url('./assets/images/hero-bg.jpg')",
+      },
+      fontFamily: {
+        'darker-grotesque': ['Darker Grotesque', 'sans-serif'],
+      },
+      screens: {
+        xsm: '475px',
+      },
+    },
+  },
+};
