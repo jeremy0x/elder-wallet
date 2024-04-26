6c552163e664df074730631dce1a0f214da8337e
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2024-04-26 14:22:32 +0100

    feat: add navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,37 @@ tailwind.config = {
     },
   },
 };
+
+// Mobile Navigation Menu
+document.addEventListener('DOMContentLoaded', function () {
+  const mobileNavbar = document.getElementById('mobile-navbar');
+  const openButton = document.getElementById('menu-button');
+  const closeButton = document.getElementById('close-button');
+  const navLinks = document.querySelectorAll('#mobile-navbar a');
+
+  openButton.addEventListener('click', function () {
+    mobileNavbar.style.transform = 'translateY(0)';
+  });
+
+  closeButton.addEventListener('click', function () {
+    mobileNavbar.style.transform = 'translateY(-100%)';
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', function () {
+      mobileNavbar.style.transform = 'translateY(-100%)';
+    });
+  });
+
+  const navbar = document.getElementById('navbar');
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      navbar.style.backgroundColor = '#13072d';
+    } else {
+      navbar.style.backgroundColor = 'transparent';
+    }
+  };
+});
